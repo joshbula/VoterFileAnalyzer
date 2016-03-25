aae2f647f9e7f690dd223751f0c0382d1b031434
Josh Bula <Josh@fmea.local>
2016-03-25 12:03:30 -0400

    Added Excel Export using EPPlus

---
+++ VoterFileAnalyzer/Helpers/ExcelExporter.cs
@@ -0,0 +1,57 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Table;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoterFileAnalyzer
+{
+    class ExcelExporter
+    {
+        public static void ExcelGenerator(DataTable dataToExcel, string excelSheetName, string FilePath)
+        {
+
+            //Uses the EPPlus Package:  http://epplus.codeplex.com/downloads/get/418378
+            //This is adapted from http://bytesofcode.hubpages.com/hub/Export-DataSet-and-DataTable-to-Excel-2007-in-C
+          
+
+
+            var newFile = new FileInfo(FilePath);
+
+            //Step 1 : Create object of ExcelPackage class and pass file path to constructor.
+            using (var package = new ExcelPackage(newFile))
+            {
+                //Step 2 : Add a new worksheet to ExcelPackage object and give a suitable name
+
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(excelSheetName);
+
+                //Step 3 : Start loading datatable from A1 cell of worksheet.
+                worksheet.Cells["A1"].LoadFromDataTable(dataToExcel, true, TableStyles.None);
+                worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+
+                //Look for DateTime columns and set them appropriately
+                var rowCount = dataToExcel.Rows.Count;
+                var dateColumns = from DataColumn d in dataToExcel.Columns
+                                  where d.DataType == typeof(DateTime) || d.ColumnName.Contains("Date")
+                                  select d.Ordinal + 1;
+                foreach (var dc in dateColumns)
+                {
+                    worksheet.Cells[2, dc, rowCount + 1, dc].Style.Numberformat.Format = "mm/dd/yyyy";
+                }
+
+
+                //Step 4 : (Optional) Set the file properties like title, author and subject
+                package.Workbook.Properties.Title = "Florida Voter File Analyzer";
+                package.Workbook.Properties.Author = "by Josh Bula";
+                package.Workbook.Properties.Subject = "";
+
+                //Step 5 : Save all changes to ExcelPackage object which will create Excel 2007 file.
+                package.Save();
+            }
+        }
+    }
+}
--- VoterFileAnalyzer/MainPage.xaml.cs
+++ VoterFileAnalyzer/MainPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +30,60 @@ namespace VoterFileAnalyzer
         private void Import_Click(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new ImportPage());
+        }
+
+        /// <summary>
+        /// Exports the Members table to an excel file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+            var fd = new SaveFileDialog();
+            fd.Filter = "Excel 2007+ Files (*.xlsx)|*.xlsx";
+            if (fd.ShowDialog() == true)
+            {
+                string FilePath = fd.FileName;
+                if (!FilePath.EndsWith(".xlsx"))
+                {
+                    FilePath += ".xlsx";
+                }
+                using (var db = new VoterFileContext())
+                {
+                    DataSet ds = new DataSet();
+                    DataTable dt = ds.Tables.Add("Members");
+                    string[] columns = typeof(Member).GetProperties().Select(p => p.Name).ToArray();
+                    foreach (var column in columns)
+                    {
+                        dt.Columns.Add(column);
+                    }
+
+                    var Members = db.Members.OrderBy(p => p.LastName);
+                    foreach (var m in Members)
+                    {
+                        DataRow r = dt.NewRow();
+                        r["FMEAID"] = m.FMEAID;
+                        r["FirstName"] = m.FirstName;
+                        r["LastName"] = m.LastName;
+                        r["HomeCounty"] = m.HomeCounty;
+                        r["HomeCity"] = m.HomeCity;
+                        r["HomeEmail"] = m.HomeEmail;
+                        r["RegisteredTovote"] = m.RegisteredTovote;
+                        r["Party"] = m.Party;
+                        r["VoterID"] = m.VoterID;
+                        r["TotalVotes"] = m.TotalVotes;
+                        r["FirstElection"] = m.FirstElection;
+                        r["LastElection"] = m.LastElection;
+
+                        dt.Rows.Add(r);
+
+                    }
+
+                    ExcelExporter.ExcelGenerator(dt, "Members", FilePath);
+                }
+
+                MessageBox.Show("Excel File Saved");
+            }
         }
     }
 }
